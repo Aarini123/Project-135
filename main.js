5f1c7d12fb09cc97591dd471df52748d043d22c0
Aarini123 <77624143+Aarini123@users.noreply.github.com>
2021-08-05 17:17:49 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+objects=[];
 video="";
 function preload(){
 }
@@ -11,7 +12,37 @@ function setup(){
 
 function draw(){
     image(video,0,0,580,600);
+    if(status!=""){
+        objectDetector.detect(gotResult);
+        nam=document.getElementById("input").value;
+        for(i=0; i<objects.length; i++){
+if(nam==objects[i].label){
+            document.getElementById("Status").innerHTML="Status:" + "Objects Detected";
+            document.getElementById("Num_obj").innerHTML="Number of objects:" + "" + objects.length;
+         
+            percent=floor(objects[i].confidence * 100);
+            R=random(255);
+            G=random(255);
+            B=random(255);
+            fill(R,G,B);
+        text(objects[i].label + "" + percent + "%",objects[i].x +15,objects[i].y+15);
+        noFill();
+        stroke(R,G,B);
+        rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+        }
+    }
 }
+}
+function gotResult(error,results){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(results);
+        objects=results;
+    }
+}
+
 
 function Start(){
     objectDetector=ml5.objectDetector("cocossd",modelLoaded);
